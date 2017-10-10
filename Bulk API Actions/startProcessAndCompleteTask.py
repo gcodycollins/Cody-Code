38284bea20be5e92ef0fdd4c607057c6da82cf92
gcodycollins <gcodycollins@gmail.com>
2017-10-10 10:58:48 -0400

    minor count updates

--- Bulk API Actions/startProcessAndCompleteTask.py
+++ Bulk API Actions/startProcessAndCompleteTask.py
@@ -23,72 +23,78 @@ import time
 
 tasks = []
 
+#loop to batch execution 
+for h in range(0,500):
 
-#API 1 start the processes
-for i in range(0,5):
- 
-    # API 1 start process
-    url = "http://54.175.46.136:8080/activiti-app/api/enterprise/process-instances"
+    #API 1 start the processes
+    for i in range(0,1000):
+     
+        # API 1 start process
+        url = "http://54.175.46.136:8080/activiti-app/api/enterprise/process-instances"
 
-    headers = {
+        headers = {
+            'content-type': "application/json"
+            }
+
+        payload = "{\r\n\"processDefinitionId\": \"histLoader:1:4\",\r\n\"businessKey\" : \"histLoader\"\r\n}"
+
+        response = requests.request("POST", url, headers=headers, data=payload, auth=HTTPBasicAuth('admin@app.activiti.com','admin'))
+
+        print(response.text)
+
+        
+        
+        
+    # API 2 get all currently running tasks
+    url2 = "http://54.175.46.136:8080/activiti-app/api/enterprise/tasks/query"
+
+    headers2 = {
         'content-type': "application/json"
         }
+            
+    payload2 = "{\n\t\"appDefinitionId\":1\n}"
+            
+    response2 = requests.request("POST", url2, headers=headers2, data=payload2, auth=HTTPBasicAuth('admin@app.activiti.com','admin'))
 
-    payload = "{\r\n\"processDefinitionId\": \"histLoader:3:61\",\r\n\"businessKey\" : \"histLoader\"\r\n}"
+    print(response2.text)
 
-    response = requests.request("POST", url, headers=headers, data=payload, auth=HTTPBasicAuth('admin@app.activiti.com','admin'))
-
-    print(response.text)
-
-    
-    
-    
-# API 2 get all currently running tasks
-url2 = "http://54.175.46.136:8080/activiti-app/api/enterprise/tasks/query"
-
-headers2 = {
-    'content-type': "application/json"
-    }
+    jData = json.loads(response2.content)
         
-payload2 = "{\n\t\"appDefinitionId\":2\n}"
+       
+
+    # Add all results of get running task api into a list. also used index exception to avoid index out of bounds once last user is reached.
+    j = 0
+    end = False
+
+    while (end!=True):
+        try:
+            tasks.append(str(jData['data'][j]['id']))
+            j+=1
+        except IndexError:
+            end=True
         
-response2 = requests.request("POST", url2, headers=headers2, data=payload2, auth=HTTPBasicAuth('admin@app.activiti.com','admin'))
 
-print(response2.text)
-
-jData = json.loads(response2.content)
-    
-   
-
-# Add all results of get running task api into a list. also used index exception to avoid index out of bounds once last user is reached.
-j = 0
-end = False
-
-while (end!=True):
-    try:
-        tasks.append(str(jData['data'][j]['id']))
-        j+=1
-    except IndexError:
-        end=True
-    
-
-#taskString= ' '.join(tasks)
-#print ("taskString: "+taskString)
+    #taskString= ' '.join(tasks)
+    #print ("taskString: "+taskString)
 
 
 
 
-#API 3 complete the processes using
-for i in range(0,len(tasks)):
- 
-    # API 3 complete task
-    url3 = "http://54.175.46.136:8080/activiti-app/api/enterprise/tasks/"+tasks[i]+"/action/complete"
+    #API 3 complete the processes using
+    for i in range(0,len(tasks)):
+     
+        # API 3 complete task
+        url3 = "http://54.175.46.136:8080/activiti-app/api/enterprise/tasks/"+tasks[i]+"/action/complete"
 
-    headers3 = {
-        'content-type': "application/json",
+        headers3 = {
+            'content-type': "application/json",
 
-    }
+        }
 
-    response3 = requests.request("PUT", url3, headers=headers3, auth=HTTPBasicAuth('admin@app.activiti.com','admin'))
+        response3 = requests.request("PUT", url3, headers=headers3, auth=HTTPBasicAuth('admin@app.activiti.com','admin'))
 
-    print(response3.text)
+        print(response3.text)
+
+    print("###########################")
+    print(str(h))
+    print("###########################")
