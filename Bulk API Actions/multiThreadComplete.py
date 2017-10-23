e566d7c41346f0219755d129cff655ee8d8acff2
gcodycollins <gcodycollins@gmail.com>
2017-10-23 10:04:51 -0400

    updated multi threaded python

--- Bulk API Actions/multiThreadComplete.py
+++ Bulk API Actions/multiThreadComplete.py
@@ -18,73 +18,84 @@ import threading
 import requests
 from requests.auth import HTTPBasicAuth
 import json
+#from Queue import Queue
 
-#tasksT = []
+threadnum=11
 
-#function for completing set of processes in thread
-def form_thread ( count):
-    tasks = []
-    
-    # API 1 get all currently running tasks in batches of 100. use count for pagination
-    url = "http://54.167.179.17:8080/activiti-app/api/enterprise/tasks/query"
+#loop to define how many times to make the multi-threaded requests
+for k in range (0,1):
 
-    headers = {
-        'content-type': "application/json"
-    }
-            
-    payload = "{\n\"appDefinitionId\":1001,\n\"size\":\"100\",\n\"start\":"+str(count)+",\n\"state\":\"active\"\n}"
-            
-    response = requests.request("POST", url, data=payload, headers=headers, auth=HTTPBasicAuth('admin@app.activiti.com','admin'))
+    #this will be a list of lists
+    tasksT = []
 
-    #print(response.text)
+    #loop to load tasksT with lits of tasks. 
+    for i in range (0,threadnum):
 
-    jData = json.loads(response.content)
+        #this is a list of all task IDs in the below api call
+        tasks = []
         
-       
+        # API 1 get all currently running tasks in batches of 100. use count for pagination
+        url = "http://54.167.179.17:8080/activiti-app/api/enterprise/tasks/query"
 
-    # Add all results of get running task api into a list. also used index exception to avoid index out of bounds once last task is reached.
-    j = 0
-    end = False
-
-    while (end!=True):
-        try:
-            tasks.append(str(jData['data'][j]['id']))
-            j+=1
-        except IndexError:
-            end=True
-
-            
-    #tasksT.append(tasks)
-
-    #API 2 complete the processes using the tasks list
-    for i in range(0,len(tasks)):
-     
-        # API 2 complete task
-        url2 = "http://54.167.179.17:8080/activiti-app/api/enterprise/tasks/"+tasks[i]+"/action/complete"
-
-        headers2 = {
-            'content-type': "application/json",
-
+        headers = {
+            'content-type': "application/json"
         }
+                
+        payload = "{\n\"appDefinitionId\":1001,\n\"size\":\"100\",\n\"start\":"+str(i)+",\n\"state\":\"active\"\n}"
+                
+        response = requests.request("POST", url, data=payload, headers=headers, auth=HTTPBasicAuth('admin@app.activiti.com','admin'))
 
-        response2 = requests.request("PUT", url2, headers=headers2, auth=HTTPBasicAuth('admin@app.activiti.com','admin'))
+        #print(response.text)
 
-        
-        #if (i==99):
-            #print(response2.text)
-        
-        
-    print("######################")
-    print(str(count)+" thread complete")
-    print("######################")
-    
-    
-threads = []
+        jData = json.loads(response.content)
+            
+           
 
-for i in range(0,2):
-    
-    t = threading.Thread(target=form_thread(i))
-    threads.append(t)
-    t.start()
-    
-    
+        # Add all results of get running task api into a list. also used index exception to avoid index out of bounds once last task is reached.
+        j = 0
+        end = False
+
+        while (end!=True):
+            try:
+                tasks.append(str(jData['data'][j]['id']))
+                j+=1
+            except IndexError:
+                end=True
+
+        #add the list to the list of lists
+        tasksT.append(tasks)
+
+    #function for completing set of processes in thread
+    def form_thread ( count, taskList):
+
+        #API 2 complete the processes using the tasks list
+        for i in range(0,len(tasks)):
+         
+            # API 2 complete task
+            url2 = "http://54.167.179.17:8080/activiti-app/api/enterprise/tasks/"+taskList[i]+"/action/complete"
+
+            headers2 = {
+                'content-type': "application/json",
+
+            }
+
+            response2 = requests.request("PUT", url2, headers=headers2, auth=HTTPBasicAuth('admin@app.activiti.com','admin'))
+
+            
+            #if (i==99):
+                #print(response2.text)
+            
+            
+        print("######################")
+        print(str(count)+" thread complete")
+        print("######################")
+        
+        
+    threads = []
+
+    #multithreaded processing
+    for i in range(0,threadnum):
+        
+        t = threading.Thread(target=form_thread(i, tasksT[i]))
+        threads.append(t)
+        t.start()
